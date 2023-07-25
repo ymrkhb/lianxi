bcaae3ee825c701e7016c094d02d6a1c0aedddcc
ymrkhb <2492930712@qq.com>
2023-07-25 15:22:34 +0800

    no message

---
+++ Ch61Application.java
@@ -0,0 +1,12 @@
+package com.ch6_1.ch6_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch61Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch61Application.class, args);
+    }
+}
